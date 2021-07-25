d2fac2301bb8cad754111b98e072846338e2b766
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-07-25 23:08:30 +0000

    Created assertEqual.js and played around with it

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(" ✅✅✅ Assertion Passed:" + actual + " === "  + expected);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed:" + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("I am a cat", "I am a cat");
+assertEqual("I am a cat", "I am a dog");
+assertEqual(3,4);
