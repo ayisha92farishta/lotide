7f97dd99293bc7d59dd256c7b0a1acd68e7f7ed0
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-07-26 19:01:42 +0000

    Completed the head function, included assertEqual for checking

--- head.js
+++ head.js
@@ -1,25 +1,20 @@
 // FUNCTION IMPLEMENTATION
-/*
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(` ✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("I am a cat", "I am a cat");
-assertEqual("I am a cat", "I am a dog");
-assertEqual(3,4); 
-*/
 
 const head = function (array) {
   firstElement = array[0];
   return firstElement;
 }
 
-console.log(head([]));
-console.log(head(["Hello", "Lighthouse", "Labs"]));
+assertEqual(head([5,6,7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
