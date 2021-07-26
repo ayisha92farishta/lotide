3304312c223028efb569cd28391517bd67dc13d8
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-07-26 21:10:54 +0000

    added function in tail.js, and did some linting for head.js and tail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,18 @@
-x = 12;
-if(true) {
-  x + 2;
-  x = 4;
-}
-console.log(x);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  array.shift();
+  return array;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
