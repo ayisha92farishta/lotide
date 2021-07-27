9c2d1718078eae4ed4c26e2f396c505cc72d8d72
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-07-27 16:32:57 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(arrayOne, arrayTwo) {
+  if (arrayOne === arrayTwo) {
+    console.log(` ✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else if (arrayOne !== arrayTwo) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+
+};
+
+// create a nested loop to see if each item in the array match exactly with the other.
+
+const eqArrays = function (arrayOne, arrayTwo){
+  let output = 0;
+  for(let i = 0; i< arrayOne.length; i++) {
+    if(arrayOne[i] === arrayTwo[i]){
+      output++
+    }
+    
+  }if(output === arrayOne.length && output === arrayTwo.length){
+    return true;
+  } else {
+    return false
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false) // => false
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3', '4']), false);
