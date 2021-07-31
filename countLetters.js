12802ab4582ca5c7ff1ca71fd6a989a70ca2569a
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-07-31 17:34:34 +0000

    Added the countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+//go over the strings
+//trim the whitespace
+//add a counter
+//check if there is any duplicate of one letter
+// if so then the add it
+
+const countLetters = function(str){
+  let result = {};
+  let withoutSpace = str.replace(/\s/g, '');
+  //console.log(withoutSpace)
+  for(let i = 0; i <withoutSpace.length; i++) {
+    let currentLetter = withoutSpace[i];
+    if (currentLetter in result){
+      continue
+    }
+    result[currentLetter] = 0;
+    for (let j = i; j < withoutSpace.length; j++){
+      if (currentLetter === withoutSpace[j]){
+        result[currentLetter] += 1;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(countLetters(" lighthouse in the house "))
+
